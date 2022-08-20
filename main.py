aa9109b0c62491db6ae6e39b8d9b226e27fe39a4
Clockknight <tylerf2wang@gmail.com>
2022-08-19 23:51:07 -0700

    implement writeJson() to save each relation to a server-specific json

--- main.py
+++ main.py
@@ -16,12 +16,14 @@ bot = commands.Bot(intents=intents, command_prefix='$', sync_commands_debug=True
 
 @bot.slash_command(description="A user will be given role1 if they ever get role2.")
 async def hierarchize(inter, role1: str, role2: str):
+    # TODO Check if user has correct permissions to do this
+    # Probably looking for admin perms + ability to assign/remove roles
     role1, role2, invalidrolebool = variableCheck(inter, role1, role2)
     if invalidrolebool:
         await inter.response.send_message('no')
     else:
-        tstring = '{} {}'.format(role1.id, role2.id)
-        await inter.response.send_message(tstring)
+        status = 'Saved! Now people will be given {} if they get {}'.format(role1, role2)
+        await inter.response.send_message(status)
         relationDefine(inter.channel.guild.id, role1, role2, 1)
 
 
@@ -45,6 +47,7 @@ async def noncompatible(inter, role1: str, role2: str):
 
 @bot.event
 async def on_member_update(before, after):
+    # Make this update roles according to roles on readJson
     if len(before.roles) < len(after.roles):
         newRole = next(role for role in after.roles if role not in before.roles)
         if newRole.name == "Respected":
@@ -54,13 +57,13 @@ async def on_member_update(before, after):
 def variableCheck(inter, r1, r2):
     """Given the context of a command, and the raw string of two roles':
     """
-    r1 = roleCheck(inter, r1)
-    r2 = roleCheck(inter, r2)
+    r1 = findRole(inter, r1)
+    r2 = findRole(inter, r2)
     invalidrolebool = r1 == '' and r2 == ''
     return r1, r2, invalidrolebool
 
 
-def roleCheck(inter, roleid):
+def findRole(inter, roleid):
     """Given command context and the raw value of a role, return the value given if it matches a role on the server.
     Else, return an empty string."""
     roleid = roleid[3:-1]
@@ -80,24 +83,46 @@ def checkRelationParadox(inter):
 
 
 def relationDefine(guildid, role1, role2, relationid):
-    relation = {role1.id: {role2.id: relationid}}
-    print(relation)
-    writeJson(guildid, relation)
+    relation = {str(role1.id): {str(role2.id): relationid}}
+    updateJson(guildid, relation)
 
 
-def writeJson(gid, newrelation):
+def updateJson(gid, newrelation):
     """Given the id of a guild and a dictionary object that represents a new relation between two roles:
     Ensure no paradox is created, with checkRelationParadox(),
     then update the json file with the dictionary object."""
-    jsonDir = './guilds/{}.json'.format(gid)
-    if not os.path.exists(jsonDir):
+
+    # Use readJson variables later to write to Json
+
+    jsondir, jsoncontents = readJson(gid)
+
+    # Get the key from the new relation and the stored relations
+    key = list(newrelation.keys())[0]
+    keys = list(jsoncontents.keys())
+
+    # Check if role has already been interacted with role
+    if key in keys:
+        jsoncontents[key].update(newrelation[key])
+    else:
+        jsoncontents.update(newrelation)
+
+    f = open(jsondir, 'w')
+    f.write(json.dumps(jsoncontents, indent=4))
+    f.close()
+
+
+def readJson(gid):
+    jsondir = './guilds/{}.json'.format(gid)
+    if not os.path.exists(jsondir):
         os.makedirs('./guilds')
-        f = open(jsonDir, 'w+')
+        f = open(jsondir, 'w+')
         f.write('{\n}')
         f.close()
-    f = open(jsonDir, 'r+')
-    thing = json.load(f)
-    # TODO finish writeJson
+    f = open(jsondir, 'r')
+    jsoncontents = json.load(f)
+    f.close()
+
+    return jsondir, jsoncontents
 
 
 bot.run(TOKEN)

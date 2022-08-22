2160a70d54284097e795cdc580cc275a7a2c8c6f
Clockknight <tylerf2wang@gmail.com>
2022-08-21 23:50:24 -0700

    Progress on updateRoles() logic

--- main.py
+++ main.py
@@ -14,52 +14,55 @@ bot = commands.Bot(intents=intents, command_prefix='$', sync_commands_debug=True
 # bot = commands.Bot(intents=intents, command_prefix='$', test_guilds=[996239355704258590])
 
 
-@bot.slash_command(description="A user will be given role1 if they ever get role2.")
-async def hierarchize(inter, role1: str, role2: str):
+@bot.slash_command(description="A user will be given parent if they ever get child.")
+async def hierarchize(inter, child: str, parent: str):
+    await relationLogic(inter, child, parent, 1)
+
+
+@bot.slash_command(description="A user will be given role1 if they get role2, or vice-versa.")
+async def match(inter, role1: str, role2: str):
+    await relationLogic(inter, role1, role2, 2)
+
+
+@bot.slash_command(description="Someone can ONLY have role1 or role2.")
+async def noncompatible(inter, role1: str, role2: str):
+    await relationLogic(inter, role1, role2, 3)
+
+
+@bot.event
+async def on_member_update(before, after):
+    newRole = None
+    if len(before.roles) < len(after.roles):
+        newRole = next(role for role in after.roles if role not in before.roles)
+    elif len(before.roles) > len(after.roles):
+        newRole = next(role for role in before.roles if role not in after.roles)
+    if newRole is not None:
+        updateRoles(newRole, members=after)
+
+
+async def relationLogic(inter, role1: str, role2: str, relationclass: int):
+    """Whenever one of the slash commands for defining roles is called, this function processes the actual logic
+    of storing all the information, since each one is essentially the same process.
+    """
     # TODO Check if user has correct permissions to do this
     # Probably looking for admin perms + ability to assign/remove roles
     role1, role2, invalidrolebool = variableCheck(inter, role1, role2)
     if invalidrolebool:
         await inter.response.send_message('no')
-    else:
-        status = 'Saved! Now people will be given {} if they get {}'.format(role1.mention, role2.mention)
-        await inter.response.send_message(status)
-        relationDefine(inter.channel.guild.id, role1, role2, 1)
-
-
-@bot.slash_command(description="A user will be given role1 if they get role2, or vice-versa.")
-async def match(inter, role1: str, role2: str):
-    role1, role2, invalidrolebool = variableCheck(inter, role1, role2)
-    if invalidrolebool:
-        await inter.response.send_message('no')
-    else:
-        await inter.response.send_message('k')
-
-
-@bot.slash_command(description="Someone can ONLY have role1 or role2.")
-async def noncompatible(inter, role1: str, role2: str):
-    role1, role2, invalidrolebool = variableCheck(inter, role1, role2)
-    if invalidrolebool:
-        await inter.response.send_message('no')
-    else:
-        await inter.response.send_message('k')
-
-
-@bot.event
-async def on_member_update(before, after):
-    # Make this update roles according to roles on readJson
-    if len(before.roles) < len(after.roles):
-        newRole = next(role for role in after.roles if role not in before.roles)
-        if newRole.name == "Respected":
-            print('ping')
+    if relationDefine(inter.channel.guild.id, role1, role2, relationclass):
+        await inter.response.send_message('nah')
+    status = 'Saved! Now people will be given {} if they get {}'.format(role1.mention, role2.mention)
+    await inter.response.send_message(status)
 
 
 def variableCheck(inter, r1, r2):
     """Given the context of a command, and the raw string of two roles':
+    Return the role objects in the server the command was sent from, using guild.get_role() from Disnake
+    And return true if either is None
     """
     r1 = findRole(inter, r1)
     r2 = findRole(inter, r2)
-    invalidrolebool = r1 == '' and r2 == ''
+    invalidrolebool = r1 is None or r2 is None
     return r1, r2, invalidrolebool
 
 
@@ -76,15 +79,24 @@ def checkRelationParadox(relation):
     Return true if a paradox is detected.
     Else, return false.
     """
+    # TODO implement checking for paradoxes
     print(relation)
 
 
 def relationDefine(guildid, role1, role2, relationid):
+    """
+    Given two roles, the ID of the relation to be established and the guild they're in:
+    Define a dict object describing the relation, run it through checkRelationParadox()
+
+    If checkRelationParadox() returns true, immediately return True
+    Else, updateJson() with the new relation, then updateRoles() on the server, then return False.
+    """
     relation = {str(role1.id): {str(role2.id): relationid}}
-    if not checkRelationParadox(relation):
-        jsoncontents = updateJson(guildid, relation)
-    else:
+    if checkRelationParadox(relation):
         return True
+    jsoncontents = updateJson(guildid, relation)
+    updateRoles(role1, jsoncontents=jsoncontents)
+    return False
 
 
 def updateJson(gid, newrelation):
@@ -105,6 +117,7 @@ def updateJson(gid, newrelation):
     # Check if role has already been interacted with role
     if key in keys:
         jsoncontents[key].update(newrelation[key])
+    # If the key from new relation doesn't already exist, update the whole json instead
     else:
         jsoncontents.update(newrelation)
 
@@ -124,7 +137,8 @@ def readJson(gid):
     """
     jsondir = './guilds/{}.json'.format(gid)
     if not os.path.exists(jsondir):
-        os.makedirs('./guilds')
+        if not os.path.exists('./guilds'):
+            os.makedirs('./guilds')
         f = open(jsondir, 'w+')
         f.write('{\n}')
         f.close()
@@ -135,9 +149,66 @@ def readJson(gid):
     return jsoncontents, jsondir
 
 
-def updateRoles(role, jsoncontents=None):
-    if jsoncontents == None:
-        jsoncontents = readJson(role.guild)
+def updateRoles(role, member=None, jsoncontents=None):
+    """Given a role, update it to follow all relations stored in the guild specific JSON
+    member, is member object to update.
+    If not provided, then this function will update the role for all users on the server.
+    jsoncontents, must be a dict of the guild-specific json. If not provided, then readJson() is called.
+    """
+
+    key = str(role.id)
+    guild = role.guild
+    addroles = []
+    removeroles = []
+    if member is None:
+        member = guild.members
+    else:
+        member = [member]
+
+    if jsoncontents is None:
+        jsoncontents = readJson(guild.id)[0]
+
+    print('\nupdateRoles\n{}'.format(jsoncontents))
+
+    if key not in list(jsoncontents.keys()):
+        print('no')
+        return
+
+    for targetmember in member:
+        memval = 0
+        if role not in targetmember.roles:
+            memval = 8
+        for roleid in jsoncontents[key]:
+            targetrole = guild.get_role(int(roleid))
+            match jsoncontents[key][roleid] + memval:
+                case 1:
+                    print('hierierarchize')
+                case 9:
+                    print('removehierarchizechild')
+
+
+        targetmember.add_roles(addroles)
+        targetmember.remove_roles(removeroles)
+
+    # TODO make this update all roles according to the relations in the json
+    # TODO make all roles get added/removed at once
+    # Get array of all members with role
+
+    # get role1 = get role2 //hierarchize
+    # rm role1 = dont care
+
+    # get role1 = rm role2 // same either direction
+    # rm role1 = get role2
+
+    # Paired
+    # rm role1 = rm role2 // Similar to hierarchize ?
+    # get role1 = get role2
+    # also, paired/symmetrical versions of above
+
+    # TODO make a two part function that removes a specific role-to-role function
+    # put considerations for symmetrical relations
+
+    # TODO make a function that reports all relations, a simple list is fine
 
 
 bot.run(TOKEN)
